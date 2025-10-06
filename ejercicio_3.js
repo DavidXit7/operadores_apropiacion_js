0e3749939613edeb8c075845b87048f6aa289f48
David Rueda <Rueda9673@gmail.com>
2025-10-06 17:51:50 -0500

    Se realizo el ejercicio numero tres, ademas, se le agrego comentarios

---
+++ ejercicio_3.js
@@ -0,0 +1,20 @@
+//Aqui se declara las constantes
+let contador;
+
+//La constante es de 100
+contador = 100;
+
+//Se convierte la constante contador a float
+contador = parseFloat(contador)
+
+// Sumar 25 visitas adicionales
+contador += 25;
+
+// Restar 10 visitas por error de registro
+contador -= 10;
+
+// Duplicar el contador
+contador *= 2;
+
+// Mostrar el resultado final
+console.log("Valor final del contador de visitas: ", contador);
